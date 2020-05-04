31493bc4cf8c4784353dab3929d08dd44dd06b56
Bruno Paula <brunopaula@dryserv.com>
2020-05-03 20:08:23 -0400

    added a nav bar and scss

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React from "react";
-import logo from "./logo.svg";
-import "./App.css";
+// import logo from "./logo.svg";
+import "./styles/App.scss";
+import Nav from "./components/navigation/nav";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1>Welcome to DryServ</h1>
+        {/* <img src={logo} className="App-logo" alt="logo" /> */}
+        <div className="logo">Logo</div>
+        <Nav />
       </header>
     </div>
   );
---
+++ src/components/navigation/nav.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+export default function Nav() {
+  return (
+    <div className="nav-bar">
+      <a href="/">Home</a>
+      <a href="">Services</a>
+      <a href="">About</a>
+      <a href="">Contact</a>
+    </div>
+  );
+}
